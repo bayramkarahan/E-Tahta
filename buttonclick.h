dd80e75ef0405652fdbf6b624514b7a7fd722a19
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2020-12-11 14:38:15 +0300

    Add files via upload

--- buttonclick.h
+++ buttonclick.h
@@ -1,3 +1,22 @@
+/*****************************************************************************
+ *   Copyright (C) 2020 by Bayram KARAHAN                                    *
+ *   <bayramk@gmail.com>                                                     *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .          *
+ *****************************************************************************/
 #ifndef BUTTONCLICK_H
 #define BUTTONCLICK_H
 #include <iostream>
@@ -1028,15 +1047,29 @@ void MainWindow::infoButtonClick(){
                    "\n\t* Kullanıcı Ayarlarını Kaydetme ve Açma Özelliği"
                    "\n\t* Saat Gösterme-Gizleme Özelliği"
                    "\n\t* Sınavlar için Sayaç Özelliği"
-                   "\n"
-                   "\n\t         Bayram KARAHAN"
-                   "\n\tBilişim Teknolojileri Öğretmeni");
+                   "/*****************************************************************************"
+                   "\n   Copyright (C) 2020 by Bayram KARAHAN                                     "
+                   "\n   <bayramk@gmail.com>  <www.bayramkarahan.blogspot.com>                                                    "
+                    "\n                                                                           "
+                    "\n   This program is free software; you can redistribute it and/or modify    "
+                    "\n   it under the terms of the GNU General Public License as published by    "
+                    "\n   the Free Software Foundation; either version 3 of the License, or       "
+                    "\n   (at your option) any later version.                                     "
+                    "\n                                                                           "
+                    "\n   This program is distributed in the hope that it will be useful,         "
+                    "\n   but WITHOUT ANY WARRANTY; without even the implied warranty of          "
+                    "\n   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           "
+                    "\n   GNU General Public License for more details.                            "
+                    "\n                                                                           "
+                    "\n   You should have received a copy of the GNU General Public License       "
+                    "\n   along with this program; if not, write to the                          "
+                    "\n   Free Software Foundation, Inc.,                                         "
+                    "\n   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .          "
+                    "\n****************************************************************************"
+
+                   );
     msgBox.setWindowTitle("E-Tahta 6.0");
-    msgBox.setInformativeText("İstek ve önerileriniz için;"
-                              "\nE-Posta: bayramk@gmail.com"
-                              "\nwww.bayramkarahan.blogspot.com"
-                              "\n");
-    // "\nÖneri ve Görüşleriyle Projeye Katkı Sağlayanlar");
+
 
     msgBox.setStandardButtons(QMessageBox::Ok);
     Qt::WindowFlags flags = 0;
